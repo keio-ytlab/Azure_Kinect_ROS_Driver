bd8f1b2089eeae67a6b5a0ed3fc9bfc6b5414cab
chakio <y-kawasaki@keio.jp>
2020-08-29 23:54:51 +0900

    FIX: dockerfile

--- include/azure_kinect_ros_driver/k4a_calibration_transform_data.h
+++ include/azure_kinect_ros_driver/k4a_calibration_transform_data.h
@@ -46,7 +46,7 @@ public:
   k4a::image transformed_depth_image_;
 
   std::string tf_prefix_ = "";
-  std::string camera_base_frame_ = "camera_base";
+  std::string camera_base_frame_ = "link";
   std::string rgb_camera_frame_ = "rgb_camera_link";
   std::string depth_camera_frame_ = "depth_camera_link";
   std::string imu_frame_ = "imu_link";
