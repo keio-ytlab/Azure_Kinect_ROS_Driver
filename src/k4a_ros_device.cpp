6f416e95f6c3d52527b8086743d68b4a0b0d4556
chakio <y-kawasaki@keio.jp>
2020-09-03 19:12:08 +0900

    WIP

--- src/k4a_ros_device.cpp
+++ src/k4a_ros_device.cpp
@@ -761,6 +761,54 @@ k4a_result_t K4AROSDevice::fillPointCloud2(const k4a::image& pointcloud_image, c
   return K4A_RESULT_SUCCEEDED;
 }
 
+k4a_result_t K4AROSDevice::fillPointCloudQuarter(const k4a::image& pointcloud_image, const k4a::image& color_image,
+                                               sensor_msgs::PointCloud2Ptr& point_cloud)
+{
+  point_cloud->height = pointcloud_image.get_height_pixels();
+  point_cloud->width = pointcloud_image.get_width_pixels();
+  point_cloud->is_dense = false;
+  point_cloud->is_bigendian = false;
+
+  const size_t point_count = pointcloud_image.get_height_pixels()/2 * pointcloud_image.get_width_pixels()/2;
+  const size_t pixel_count = color_image.get_size() / sizeof(BgraPixel);
+  if (point_count != pixel_count)
+  {
+    ROS_WARN("Color and depth image sizes do not match!");
+    return K4A_RESULT_FAILED;
+  }
+
+  sensor_msgs::PointCloud2Modifier pcd_modifier(*point_cloud);
+  pcd_modifier.setPointCloud2FieldsByString(1, "xyz");
+
+  sensor_msgs::PointCloud2Iterator<float> iter_x(*point_cloud, "x");
+  sensor_msgs::PointCloud2Iterator<float> iter_y(*point_cloud, "y");
+  sensor_msgs::PointCloud2Iterator<float> iter_z(*point_cloud, "z");
+
+  pcd_modifier.resize(point_count);
+
+  const int16_t* point_cloud_buffer = reinterpret_cast<const int16_t*>(pointcloud_image.get_buffer());
+
+  for (size_t i = 0; i < point_count; i++, ++iter_x, ++iter_y, ++iter_z)
+  {
+    int pixel_num = i;
+    // Z in image frame:
+    float z = static_cast<float>(point_cloud_buffer[3 * i + 2]);
+    if (z <= 0.0f )
+    {
+      *iter_x = *iter_y = *iter_z = std::numeric_limits<float>::quiet_NaN();
+    }
+    else
+    {
+      constexpr float kMillimeterToMeter = 1.0 / 1000.0f;
+      *iter_x = kMillimeterToMeter * static_cast<float>(point_cloud_buffer[3 * i + 0]);
+      *iter_y = kMillimeterToMeter * static_cast<float>(point_cloud_buffer[3 * i + 1]);
+      *iter_z = kMillimeterToMeter * z;
+    }
+  }
+
+  return K4A_RESULT_SUCCEEDED;
+}
+
 k4a_result_t K4AROSDevice::getImuFrame(const k4a_imu_sample_t& sample, sensor_msgs::ImuPtr& imu_msg)
 {
   imu_msg->header.frame_id = calibration_data_.tf_prefix_ + calibration_data_.imu_frame_;
